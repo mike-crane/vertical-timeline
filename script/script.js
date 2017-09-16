bdc694b61281403defd55a81fe5d4c30f7b01622
mike crane <mike.c0321@gmail.com>
2017-09-16 00:15:54 -0400

    add images and scroll animations

---
+++ script/script.js
@@ -0,0 +1,23 @@
+function isElementInViewport(e) {
+  var rect = e.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+var items = document.querySelectorAll(".folds li");
+ 
+ 
+function callbackFunc() {
+  for (var i = 0; i < items.length; i++) {
+    if (isElementInViewport(items[i])) {
+      items[i].classList.add("in-view");
+    }
+  }
+}
+ 
+window.addEventListener("load", callbackFunc);
+window.addEventListener("scroll", callbackFunc);
